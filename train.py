7717e72c645dea9d866322ef58be8600094dbbf1
huangs0 <59648015+huangs0@users.noreply.github.com>
2020-08-20 13:44:13 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ import torch.nn as nn
 import torch.utils.data.dataloader
 import torchvision.models as models
 from tensorboardX import SummaryWriter
-import Final.model as model
-import Final.DataSetLoader as DataSetLoader
+import model
+import DataSetLoader
 import time
 
 '''
@@ -24,7 +24,7 @@ Main Function:
 
 # Please Set the Training Epoch here
 batch_size = 7
-# The average memory use is 1GB-1.5GB per batch, Please refer to your GPU set the batch_size
+# The average memory use is 1GB-1.5GB per batch, Please refer to your GPU set the batch_size, Remember left some memory for abundance
 epoch_num = 51
 # The average epoch_num to achieve nearly best performance is around 100
 use_cuda = torch.cuda.is_available()
@@ -34,40 +34,36 @@ print("Is CUDA available?", use_cuda)
 print("Loading Model...")
 VGG = models.vgg16(pretrained=True)
 # url = https://download.pytorch.org/models/vgg16-397923af.pth
-Net = model.BN_Avg_DenseNet(VGG)
+Net = model.BNNet(VGG)
 '''
 Available Net:
     Net = model.OriginalNet(VGG)
     Net = model.BNNet(VGG)
     Net = model.BN_AvgNet(VGG)
-
-
-
-Copy and Paste  ：)
+    Net = model.BN_Avg_ResNet(VGG)
+    Net = model.BN_Avg_DenseNet(VGG)
+Copy and Paste XD
 '''
 print(Net)
-writer = SummaryWriter(comment="BN_Avg_Dense_Adam")
+writer = SummaryWriter(comment="xxxxxx")
 # Initialize tensorboardX, please make sure commnet is correct before start training
 if use_cuda:
     Net.cuda()
     # Convert model to cuda, type=FloatCudaTensor
 print("Finishing Loading Model")
 
-
 def criterion(output, label):
     loss = nn.CrossEntropyLoss()
     out = loss(output, label)
     return out
 
-
 optimizer = optim.Adam(Net.parameters())
-# optimizer = optim.Adam(Net.parameters())
+# optimizer = optim.SGD(Net.parameters(), lr = 0.001, momentum = 0.9, weight-decay = 1e-5)
 
 print("loading data....")
 train_data = DataSetLoader.SmokeDataSet(inputpath="train_input.npy", labelpath="train_label.npy")
 train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=5)
 print("Finishing Loading Data, Starting Training")
-
 
 def train(epoch, track):
     Net.train()
@@ -99,22 +95,22 @@ def train(epoch, track):
                                                                         len(train_loader),
                                                                         total_loss / (batch_idx + 1)))
     if (epoch) % 10 == 0:
-        torch.save(Net, 'BN_Avg_Dense_Adammodel%d.pth' % epoch)  # save for 5 epochs
-        # Please make sure the file name is correct before training or previous file will be overwrited
+        torch.save(Net, 'xxxxxxmodel%d.pth' % epoch)  # save for 5 epochs
+        # Please make sure the file name is correct before training or previous file will be overwrited !!!
         total_loss /= len(train_loader)
         print('train epoch [%d/%d] average_loss %.5f' % (epoch, epoch_num, total_loss))
 
 
-track = True
+track = False
 '''
 This variable 'track' specifies whether tracking filter (weight and bias)
 Please be careful to use this function because it will cost long time for I/O connection between CPU and GPU
-And WILL SIGNIFICANTLY SLOW DOWN THE TRAINING SPEED
-Only use this when you need to find errors in filter ：)
+And WILL SIGNIFICANTLY SLOW DOWN THE TRAINING SPEED BY 40% OR MORE
+Only use this when you need to find errors in filter XD
 '''
 for epoch in range(epoch_num):
     start = time.time()
     train(epoch, track=track)
     end = time.time()
-    writer.add_scalar("time", start - end, global_step=1)
+    writer.add_scalar("time", end - start, global_step=1)
 writer.close()
